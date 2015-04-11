17736906491f8272defb1cbea1b8ddd51b05525d
LearyBiscuit <nimand@gmail.com>
2015-04-11 10:25:55 +0300

    original commit

---
+++ logger-lib.js
@@ -0,0 +1,143 @@
+var net = require('net');
+var os = require('os');
+var dgram=require('dgram');
+//
+// node-syslog.js - TCP syslog-ng client
+//
+
+// Message severity levels
+this.LOG_EMERG = 0;
+this.LOG_ALERT = 1;
+this.LOG_CRIT = 2;
+this.LOG_ERROR = 3;
+this.LOG_WARNING = 4;
+this.LOG_NOTICE = 5;
+this.LOG_INFO = 6;
+this.LOG_DEBUG = 7;
+
+this.FACILITY_USER = 1;
+
+this.DEFAULT_OPTIONS = {
+    facility: exports.FACILITY_USER,
+    name: null,
+    debug: false
+};
+
+var hostname = os.hostname();
+
+this.Client = function (port, host, options) {
+    this.port = port || 514;
+    this.host = host || 'localhost';
+    this.options = options || {};
+
+    for (var k in exports.DEFAULT_OPTIONS) {
+        if (this.options[k] === undefined) { this.options[k] = exports.DEFAULT_OPTIONS[k] }
+    }
+
+    // We need to set this option here, in case the module is loaded before `process.title` is set.
+    if (! this.options.name) { this.options.name = process.title || process.argv.join(' ') }
+    if (this.options.hostname != "") { hostname = this.options.hostname}
+    //this.options.pid = this.options.fakepid ?
+
+    this.socket = null;
+    this.retries = 0;
+    this.queue = [];
+};
+this.Client.prototype = new(function () {
+    var that = this;
+
+    // Generate logging methods, such as `info`, `debug`, ...
+    for (var k in exports) {
+        if (/^LOG/.test(k)) {
+            (function (level, name) {
+                that[name] = function (msg) {
+                    this.log(msg, exports[level]);
+                };
+            })(k, k.match(/^LOG_([A-Z]+)/)[1].toLowerCase());
+        }
+    }
+
+    this.log = function (msg, severity) {
+        var that = this;
+        msg = msg.trim();
+        severity = severity !== undefined ? severity : exports.LOG_INFO;
+
+        if (severity === exports.LOG_DEBUG && !this.options.debug) { return }
+
+
+        this.connect(function (e) {
+
+
+            var pri = '<' + ((that.options.facility * 8) + severity) + '>'; // Message priority
+            var entry = pri + [
+                new(Date)().toJSON(),
+                hostname,
+                that.options.name + '[' + process.pid + ']:',
+                msg
+            ].join(' ') + '\n';
+
+            //
+            // If there's a connection problem,
+            // queue the message for later processing.
+            //
+            if (e) {
+                that.queue.push(entry);
+            // Write the entry to the socket
+            } else {
+                //that.socket.write(entry, 'utf8', function (e) {
+                //    if (e) { that.queue.push(entry) }
+                //});
+				var buf=new Buffer(entry,"utf-8");
+				that.socket.send(buf, 0, buf.length, that.port, that.host, function(err, bytes){
+					var emsg='';
+					if(err) {
+						emsg='error sending message';
+					}
+					else {
+						emsg=bytes+' bytes sent';
+					}
+				});
+            }
+        });
+    };
+    this.connect = function (callback) {
+        var that = this;
+
+        callback = callback || function () {};
+
+        if (this.socket) {
+			callback(null);
+        } else {
+			this.socket=dgram.createSocket('udp4');
+			this.socket.on('error',function(err){
+				that.connected = false;
+			});
+			this.socket.on('close',function(err){
+				that.connected = false;
+			});
+			this.queue.forEach(function(entry){
+				var buf=new Buffer(entry,"utf-8");
+				that.socket.send(buf, 0, buf.length, that.port, that.host, function(err, bytes){
+					var emsg='';
+					if(err) {
+						emsg='error sending message';
+					}
+					else {
+						emsg=bytes+' bytes sent';
+					}
+				});
+			});
+
+			that.queue = [];
+			that.retries = 0;
+			that.connected = true;
+
+			callback(null);
+        }
+    };
+});
+
+this.createClient = function (port, host, options) {
+    return new(this.Client)(port, host, options);
+};
+
---
+++ logs-gen.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+/**
+ * random syslog messages generator for McAfee SIEM
+ *
+ * services supported: ssh2
+ *
+ * Valid login string looks like
+ * Mar 31 17:11:21 localhost sshd[65858]: Accepted password for root from 172.16.4.105 port 50468 ssh2
+ *
+ * Wrong password string looks like
+ * Mar 31 17:15:29 localhost sshd[65897]: Failed password for root from 172.16.4.105 port 50669 ssh2
+ *
+ * Another appeared message
+ * Mar 31 17:15:23 localhost sshd[65858]: Received disconnect from 172.16.4.105: 11: disconnected by user
+ *
+ * TODO
+ *  - imitate the bruteforce attack on the ssh server (single remote IP, a lot of wrong logins)
+ *  - add more services
+ */
+
+var strftime = require('strftime');
+var syslog = require('./logger-lib');
+
+var   syslogServerHost = "192.168.201.160" // syslog target host
+    , syslogServerPort = 514 // syslog port, we will use UDP
+    , users = ['root', 'admin', 'ramon', 'andy', 'h4ck3r', 'nimda', 'toor', 'billg', 'steve', 'justauser', 'operator', 'monitoring', 'antiram', 'vlad', 'oleg', 'igor3']; // username to appear in the log string
+var options = {};
+
+options.name = 'sshd';
+options.hostname = 'very_important_ssh_server';
+
+var logger = syslog.createClient(syslogServerPort, syslogServerHost, options);
+
+var getRandomX = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+var loginOK = function() { // whether login is failed or not
+    return getRandomX(1,50) > 2; // this will give about 5% of failed logins
+};
+
+var getRemotePort = function () {
+    return getRandomX(10000, 65534);
+};
+
+var getRemoteHost = function () {
+    var   octet = [];
+
+    for (var i = 0; i < 4; i++) {
+        octet[i] = getRandomX(1, 219);
+    }
+    return octet[0] + "." + octet[1] + "." + octet[2] + "." + octet[3];
+};
+
+/*
+var getProcessID = function () {
+    return getRandomX(1500, 65534);
+};
+
+var getDate = function () {
+    // format is as follows: Mar 31 17:15:23
+    return strftime('%b %d %H:%M:%S');
+};
+*/
+
+var getUser = function () {
+    return users[getRandomX(0,users.length)];
+};
+
+var msgLoginOK = function () {
+    //return getDate() + ' localhost sshd[' + getProcessID() +']: Accepted password for ' + getUser() +
+    return  'Accepted password for ' + getUser() + ' from ' + getRemoteHost() + ' port ' + getRemotePort() + ' ssh2';
+};
+
+var msgLoginFailed = function () {
+    return 'Failed password for ' + getUser() + ' from ' + getRemoteHost() + ' port ' + getRemotePort() + ' ssh2';
+};
+
+var sendMessageToSyslog = function (message) {
+    //
+    console.log(message);
+    logger.info(message);
+};
+
+var sleep = function (milliseconds) {
+    var start = new Date().getTime();
+    for (var ms = 0; ms < 1e7; ms++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+};
+for (var ty=1; ty<10000; ty++) {
+    loginOK() ? sendMessageToSyslog(msgLoginOK()) : sendMessageToSyslog(msgLoginFailed());
+    sleep(1000);
+}
